2cc7136f1a705d39f6cd54f86bd939e3f063e466
fantasy <515514053@qq.com>
2021-01-08 14:14:52 +0800

    admin update to vite2

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,4 +1,5 @@
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { getServerIp } from './src/utils/helper';
 
@@ -6,36 +7,39 @@ const pathResolve = function (dir: string) {
     return resolve(__dirname, dir);
 };
 
-const viteConfig: UserConfig = {
-    // 端口号
-    port: 8999,
-    // 浏览器
-    open: false,
+export default defineConfig({
+    plugins: [vue()],
     // 别名
     alias: {
-        '/@/': pathResolve('src')
-    },
-    // 本地代理
-    proxy: {
-        '/api': {
-            target: getServerIp()
-        }
+        '/@': pathResolve('src')
     },
     // 配置Dep优化行为
     // 会使用 rollup 对 包重新编译，将编译成符合 esm 模块规范的新的包放入 node_modules 下的 .
     optimizeDeps: {
         include: ['echarts', 'echarts/map/js/china']
     },
-    cssPreprocessOptions: {
-        less: {
-            modifyVars: {
-                'primary-color': '#5d5bff',
-                'link-color': '#5d5bff',
-                'border-radius-base': '2px'
-            },
-            javascriptEnabled: true
+    css: {
+        preprocessorOptions: {
+            less: {
+                modifyVars: {
+                    'primary-color': '#5d5bff',
+                    'link-color': '#5d5bff',
+                    'border-radius-base': '2px'
+                },
+                javascriptEnabled: true
+            }
+        }
+    },
+    server: {
+        // 端口号
+        port: 8999,
+        // 浏览器
+        open: false,
+        // 本地代理
+        proxy: {
+            '/api': {
+                target: getServerIp()
+            }
         }
     }
-};
-
-export default viteConfig;
+});
